7614649e01c034e9ae8d33aabb1b54e8d22662b5
sam <sam@localhost.localdomain>
2025-08-02 12:37:19 -0400

    Build trading partner

--- main.py
+++ main.py
@@ -5,25 +5,8 @@ Sam Dawley
 08/2025
 """
 
-# misc
-import os
-import sys
-from typing import Tuple
 
-from dotenv import load_dotenv
-
-# numerics
-import numpy as np
-
-# trading
-from alpaca.trading.client import TradingClient
-
-
-# ----- global/environment variables -----
-
-load_dotenv() # defaults to ./.env
-API_KEY = os.getenv("ALPACA_API_KEY")
-API_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
+from src.trade import TradeConfig
 
 # ==================================================
 # MAIN ROUTINES
@@ -37,9 +20,10 @@ def main() -> int:
 
     # ----- trading client -----
 
-    trade_client = TradingClient(api_key=API_KEY, secret_key=API_SECRET_KEY, paper=True)
-    account_data = trade_client.get_account()
-    print('${} is available as buying power.'.format(account_data.buying_power))
+    trader = TradeConfig()
+    trader.get_account()
+
+
 
     return 0
 
---
+++ src/trade.py
@@ -0,0 +1,56 @@
+"""
+Classes used for initialized trader
+
+Sam Dawley
+08/2025
+"""
+import os
+
+from dotenv import load_dotenv
+
+# trading
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import GetAssetsRequest
+from alpaca.trading.enums import AssetClass
+
+# ==================================================
+# TRADER
+# ==================================================
+
+class TradeConfig:
+
+    # ===== Initialization =====
+
+    def __init__(self, api_key: str | None = None, api_secret_key: str | None = None, paper: bool = True) -> None:
+        
+        if isinstance(api_key, type(None)) or isinstance(api_secret_key, type(None)):
+            load_dotenv()
+            self.api_key = os.getenv("ALPACA_API_KEY")
+            self.api_secret_key = os.getenv("ALPACA_SECRET_KEY")
+
+        else:
+            self.api_key = api_key
+            self.api_secret_key = api_secret_key
+
+        self.trader = TradingClient(self.api_key, self.api_secret_key, paper=paper)
+
+        return 
+    
+    def __repr__(self) -> str:
+        return f"Trading Partner"
+    
+
+    # ===== private functions =====
+
+    # ===== public functions =====
+
+    def get_account(self) -> None:
+        for a in self.trader.get_account():
+            print(f"{a[0]:<30s}: {a[1]}")
+
+        return
+            
+
+
+if __name__ == "__main__":
+    ...
